5e4008862d9c44d88f238cd27b622f6b16af0b2f
“AutomationOnAppium” <senthilkumar1225@outlook.com>
2023-03-27 23:41:26 +0530

    output have been updated in the Hello World Program

--- src/main/java/org/javaprograms/Task1/HelloWorld.java
+++ src/main/java/org/javaprograms/Task1/HelloWorld.java
@@ -1,6 +1,9 @@
 package org.javaprograms.Task1;
 public class HelloWorld {
+
+    // Output :- Hello World!
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");     // O/P :- Hello World!
+        System.out.println("Hello world!");
     }
 }
