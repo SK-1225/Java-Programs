cde18913ccdf7394dde483ec8e3eac5aac22fc5b
Senthil Kumar <126282629+SK-1225@users.noreply.github.com>
2023-04-06 19:25:00 +0530

    Update Printmybook.java
    Updated single inheritance implementation.

--- src/main/java/org/javaprograms/LeetCodePrograms/Printmybook.java
+++ src/main/java/org/javaprograms/LeetCodePrograms/Printmybook.java
@@ -1,5 +1,7 @@
 package org.javaprograms.LeetCodePrograms;
 
+// Used single inheritance implemenation 
+
 public class Printmybook extends Book {
     public Printmybook(String name, String author, double price) {
         super(name, author, price);
