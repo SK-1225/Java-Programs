070cc587de53f8985dba8b5c5a9a404fc72ffab8
Senthil Kumar <126282629+SK-1225@users.noreply.github.com>
2023-04-06 19:26:13 +0530

    Update RunnerTest.java
    Super function called as a class loader

--- src/main/java/org/javaprograms/LeetCodePrograms/RunnerTest.java
+++ src/main/java/org/javaprograms/LeetCodePrograms/RunnerTest.java
@@ -4,6 +4,8 @@ public class RunnerTest {
 
     public static void main(String[] args) {
 
+        // Super function called as a class loader
+        
         Book b = new Printmybook("My Life - My Way","Senthil Kumar",100);
         b.getDetails();
     }
